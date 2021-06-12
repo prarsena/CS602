ac63ad22f772573c9a07cfe911ff69502d7077a9
prarsena <prarsena@gmail.com>
2021-06-11 22:10:34 -0400

    integrated timestamps into requests

--- P4.py
+++ P4.py
@@ -18,18 +18,42 @@ midnightTwoDaysAgo = midnightYesterday - SECONDS_IN_A_DAY
 midnightThreeDaysAgo = midnightTwoDaysAgo - SECONDS_IN_A_DAY
 midnightFourDaysAgo = midnightThreeDaysAgo - SECONDS_IN_A_DAY
 
-datestamps = [int(midnightLastNight), midnightYesterday, midnightTwoDaysAgo, midnightThreeDaysAgo, midnightFourDaysAgo ]
-for i in datestamps:
-    print(i)
-    print(datetime.fromtimestamp(i).strftime('%Y-%m-%d %H:%M:%S %Z'))
+datestamps = [int(midnightLastNight), midnightYesterday, midnightTwoDaysAgo, midnightThreeDaysAgo, midnightFourDaysAgo]
+#datestamps = [int(midnightLastNight), midnightFourDaysAgo]
 
-# make 5 calls to the daily/ hourly data.
 # for i in datestamps:
-#     uri = "http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=60.99&lon=30.9&dt=1623185907&units=imperial&appid=" + apikey
-#     response = requests.request("GET", uri)
-#     print(response.text)
+#     print(i)
+#     print(datetime.fromtimestamp(i).strftime('%Y-%m-%d %H:%M:%S %Z'))
+LAT = 43.3602
+LON = -71.0551
 
-# 5 days (datestamp calls) worth of
-#list = [{}, {}, {}, {}, {}]
+LONDON_LAT = 51.5074
+LONDON_LON = -0.118092
+weatherData = []
+# make 5 calls to the daily/ hourly data.
+for date in datestamps:
+     uri = "http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=43.3602&lon=-71.0551&dt="+str(date)+\
+           "&units=imperial&appid=" + apikey
+     response = requests.request("GET", uri)
+     #print(response.text)
+     weatherData.append(response.json())
 
-# document unix datetime to python str.ftime
+# weatherData is a list, which contains 5 entries.
+# The list entries are dictionaries.
+# list = [{}, {}, {}, {}, {}]
+
+# Traverse the list:
+for dayOfWeatherData in weatherData:
+    print(dayOfWeatherData.get('current').get('dt'))
+    for hour in range(23,0,-1):
+        hourlyDatestamp = dayOfWeatherData.get('hourly')[hour].get('dt')
+        #print(hourlyDatestamp)
+        # document unix datetime to python str.ftime
+        timeInfo_utc = datetime.utcfromtimestamp(hourlyDatestamp).strftime('%Y-%m-%d %H:%M:%S %Z')
+        timeInfo_mytime = datetime.fromtimestamp(hourlyDatestamp).strftime('%Y-%m-%d %H:%M:%S %Z')
+        tempInfo = dayOfWeatherData.get('hourly')[hour].get('temp')
+
+        print("UTC: " + timeInfo_utc)
+        print("EDT: " + timeInfo_mytime)
+        print(tempInfo)
+
