d00f5b819bcf97c1eb6107e7c0f1a91861ed819d
prarsena <prarsena@gmail.com>
2021-06-12 17:40:15 -0400

    updated student

--- StudentCode.py
+++ StudentCode.py
@@ -15,28 +15,14 @@ weatherData = weather.getWeather(apikey, latitude, longitude)
 # print(pp.pprint(data))
 
 timezoneOffset = weatherData[0]['timezone_offset']
-print(timezoneOffset)
-for day in weatherData:
-    # We want the data to be ingested from hour 23 down to 0,
-    # Not this way:
-    # for hour in day['hourly']:
-    #     hourlyDateStamp = hour['dt']
-    #     hourString = datetime.utcfromtimestamp(hourlyDateStamp + timezoneOffset).strftime('%Y-%m-%d %H:%M:%S')
-    #     hourlyTemperature = hour['temp']
-    #     hourlyFeelsLike = hour['feels_like']
-    #     print("Unix time: ", hourlyDateStamp)
-    #     print("Time: " + hourString)
-    #     print("Temp: ", hourlyTemperature)
-    #     print("Feels: ", hourlyFeelsLike)
-    #     print()
 
-    # Something like this:
-    hoursInADay = len(day['hourly'])
-    for hour in range(hoursInADay - 1, -1, -1):
-        hourlyDateStamp = day['hourly'][hour]['dt']
+for day in weatherData:
+
+    for hour in reversed(day['hourly']):
+        hourlyDateStamp = hour['dt']
         hourString = datetime.utcfromtimestamp(hourlyDateStamp + timezoneOffset).strftime('%Y-%m-%d %H:%M:%S')
-        hourlyTemperature = day['hourly'][hour]['temp']
-        hourlyFeelsLike = day['hourly'][hour]['feels_like']
+        hourlyTemperature = hour['temp']
+        hourlyFeelsLike = hour['feels_like']
         print("Unix time: ", hourlyDateStamp)
         print("Time: " + hourString)
         print("Temp: ", hourlyTemperature)
